6c49dcaf2b423d21a9e9f78436a78b5b5b3b656b
RobCampbell7 <73227174+RobCampbell7@users.noreply.github.com>
2020-10-21 15:48:29 +0100

    main

--- Creative-Challenges/poetry.py
+++ Creative-Challenges/poetry.py
@@ -2,6 +2,7 @@ import syllables
 
 def make_line(num_syllables):
     # returns line with given syllables
+    return num_syllables*"a"
 
 if __name__ == "__main__":
-    
+    print("{0:^20}\n{1:^20}\n{2:^20}".format(make_line(5), make_line(7), make_line(5)))
