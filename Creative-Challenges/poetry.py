0a840a6d1e8d23f5e98df15024274d6065c53287
Ryo-not-rio <ryo.not.rio.suzuki@gmail.com>
2020-10-21 15:46:42 +0100

    finish make_line

--- Creative-Challenges/poetry.py
+++ Creative-Challenges/poetry.py
@@ -3,23 +3,30 @@ import random
 
 # Finds words with particular number of syllables
 def find_word(num_syllables):
-    with open("word_list.txt") as f:
+    with open("Creative-Challenges\word_list.txt") as f:
         syllable = float('inf')
+        r = f.readlines()
+        word = random.choice(r)
         while syllable != num_syllables:
-            word = random.choice(f.readlines())
-            word = word[:-1]
+            word = random.choice(r)
+            if word != 'yourself':
+                word = word[:-1]
             syllable = syllables.estimate(word)
         return word
 
 def make_line(num_syllables):
-    with open("word_list.txt") as f:
+    with open("Creative-Challenges\word_list.txt") as f:
         syllable = float('inf')
-        while syllable < num_syllables:
-            word = random.choice(f.readlines())
-            word = word[:-1]
+        r = f.readlines()
+        word = random.choice(r)
+        while syllable > num_syllables:
+            word = random.choice(r)
+            if word != 'yourself':
+                word = word[:-1]
             syllable = syllables.estimate(word)
     
-    word += find_word(num_syllables-syllable)
+    if num_syllables != syllable:
+        word += " " + find_word(num_syllables-syllable)
     return word
         
         
