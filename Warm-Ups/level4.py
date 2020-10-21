c681c1f4c957d10ba2452fded3efda377d472315
Ryo-not-rio <60484930+Ryo-not-rio@users.noreply.github.com>
2020-10-21 14:57:29 +0100

    finish level 4

--- Warm-Ups/level4.py
+++ Warm-Ups/level4.py
@@ -4,7 +4,13 @@ def double_swap(string, a, b):
     # Add your solution here. You can use additional functions if need be.
     # Don't forget to add a DocString for all your functions and comment your code.
     # Your functions should return values rather than printing the result although you can use printing for testing purposes.
-    return "Your Return Value"
+    string = list(string)
+    for i, s in enumerate(string):
+        if s == a:
+            string[i] = b
+        if s == b:
+            string[i] = a
+    return "".join(string)
 
 if __name__ == '__main__':
     # Add any code to test your solution here
