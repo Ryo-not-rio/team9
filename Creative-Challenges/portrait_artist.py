23d9c546b5b113e64707a603cb2fe22d6830f5c2
Ryo-not-rio <ryo.not.rio.suzuki@gmail.com>
2020-10-21 16:13:12 +0100

    comment

--- Creative-Challenges/portrait_artist.py
+++ Creative-Challenges/portrait_artist.py
@@ -1,2 +1,6 @@
 import turtle
 
+
+# Takes list of [((start_x, start_y), (end_x, end_y)), ((start_x, start_y), (end_x, end_y)), ((start_x, start_y), (end_x, end_y)) ...]
+# and draws them onto canvas
+def draw_lines(coordinates):
