719fcc1ff048481f816ccc81a45c5ccd95883121
Ryo-not-rio <ryo.not.rio.suzuki@gmail.com>
2020-10-21 18:54:32 +0100

    add beats

--- Final-challenge/beat.py
+++ Final-challenge/beat.py
@@ -1,14 +1,14 @@
 import pygame as py
 
-beat_width = 40
+beat_width = 60
 
 class Beat:
     def __init__(self, lane):
         self.lane = lane
         self.pos = [int(((800 - beat_width*4)/2 + beat_width/2) + lane*beat_width), 0]
 
-    def is_success(self, pos):
-        if abs(pos[1] - self.pos[1]) < 5:
+    def is_success(self):
+        if abs(770 - self.pos[1]) < 25:
             return True
         return False
 
