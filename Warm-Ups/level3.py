f719c5d0a907aa55239f07085fdf653bb2a8dd8e
Ryo-not-rio <60484930+Ryo-not-rio@users.noreply.github.com>
2020-10-21 15:01:25 +0100

    finish level 3

--- Warm-Ups/level3.py
+++ Warm-Ups/level3.py
@@ -1,18 +1,12 @@
-""" Hackathon - Level 4 """
+""" Hackathon - Level 3 """
 
-def double_swap(string, a, b):
-    # Add your solution here. You can use additional functions if need be.
-    # Don't forget to add a DocString for all your functions and comment your code.
-    # Your functions should return values rather than printing the result although you can use printing for testing purposes.
-    string = list(string)
-    for i, s in enumerate(string):
-        if s == a:
-            string[i] = b
-        if s == b:
-            string[i] = a
-    return "".join(string)
+def oddish_evenish(x):
+    if sum([int(i) for i in list(str(x))]) % 2 == 0:
+        return "Evenish"
+    return "Oddish"
 
 if __name__ == '__main__':
     # Add any code to test your solution here
-    # As per the example, this should return ienv, ivdv, ivcv
-    print(double_swap('veni, vidi, vici', 'v', 'i'))
+    # As per the example, this should return Oddish
+    print(oddish_evenish(1190))
+    
