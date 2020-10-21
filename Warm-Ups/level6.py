d1a5ddf4010e407983178060e163c85e965e7990
Ryo-not-rio <60484930+Ryo-not-rio@users.noreply.github.com>
2020-10-21 14:44:02 +0100

    finish level6

--- Warm-Ups/level6.py
+++ Warm-Ups/level6.py
@@ -1,13 +1,11 @@
 """ Hackathon - Level 6 """
 
-def cipher(string, x):
-    # Add your solution here. You can use additional functions if need be.
-    # Don't forget to add a DocString for all your functions and comment your code.
-    # Your functions should return values rather than printing the result although you can use printing for testing purposes.
-    return "Your Return Value"
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+def cipher(str, x):
+    return "".join([alphabet[(alphabet.index(s)+x)%26] for s in str])
 
 if __name__ == '__main__':
     # Add any code to test your solution here
     # As per the example, this should return salve
     print(cipher('pxisb', 3))
-    
