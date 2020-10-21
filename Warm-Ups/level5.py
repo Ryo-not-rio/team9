690656965fd556714600290037ea64839c6dda3b
Ryo-not-rio <60484930+Ryo-not-rio@users.noreply.github.com>
2020-10-21 15:15:32 +0100

    fix level5

--- Warm-Ups/level5.py
+++ Warm-Ups/level5.py
@@ -1,12 +1,18 @@
+  
 """ Hackathon - Level 5 """
 
 def convert(numeral):
     conv = {"I": 1, "X": 10, "C": 100, "M": 1000, "V": 5, "L": 50, "D": 500}
-    return int(sum([conv[s] for s in numeral]))
+    summed = 0
+    for i, s in enumerate(numeral):
+        temp_s = conv[s]
+        if i < len(numeral)-2:
+            if conv[numeral[i+1]] > temp_s:
+                temp_s = -temp_s
+        summed += temp_s
+    return summed
 
 if __name__ == '__main__':
     # Add any code to test your solution here
     # As per the example, this should return 1145
     print(convert('MCXLV'))
-    
-    
