3f62f50d1a53f48a5fd5482888a41b177b784a0e
Ryo-not-rio <ryo.not.rio.suzuki@gmail.com>
2020-10-21 17:55:27 +0100

    beat draw

--- Final-challenge/beat.py
+++ Final-challenge/beat.py
@@ -3,7 +3,7 @@ import pygame as py
 class Beat:
     def __init__(self, lane):
         self.lane = lane
-        self.pos = (100, 0)
+        self.pos = [100, 0]
 
     def draw(self, display):
-        py.draw.circle(display, self.pos, 20)
+        py.draw.circle(display, py.Color('blue'), self.pos, 20)
--- Final-challenge/main.py
+++ Final-challenge/main.py
@@ -1,12 +1,17 @@
 import pygame as py
 
+import beat
+
 py.init()
 
 display = py.display.set_mode((800, 800))
+clock = py.time.Clock()
 
 game_exit = False
 
-# beats = [1.9, 2.01, ...]
+beats = [1.9, 2.01]
+beat_objs = [beat.Beat(1)]
+speed = 10
 
 while not game_exit:
     for event in py.event.get():
@@ -16,9 +21,17 @@ while not game_exit:
             if event.key == py.K_RIGHT:
                 pass
 
+    for beat in beat_objs:
+        beat.pos[1] += speed
+
+
     ### Drawing ###
     display.fill(py.Color('white'))
-    py.draw.rect(display, py.Color('blue'), (0, 0, 100 ,100))
+    for beat in beat_objs:
+        beat.draw(display)
+    # py.draw.rect(display, py.Color('blue'), (0, 0, 100 ,100))
+
+    clock.tick(50)
     py.display.update()
 
 py.quit()
