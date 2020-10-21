7dd72ac3d2f3d28dddbace4d373a5790a6cb50e7
flkapes <70965394+flkapes@users.noreply.github.com>
2020-10-21 15:04:38 +0100

    level2.py
    Faris

--- Warm-Ups/level2.py
+++ Warm-Ups/level2.py
@@ -4,7 +4,10 @@ def longest_word(string):
     # Add your solution here. You can use additional functions if need be.
     # Don't forget to add a DocString for all your functions and comment your code.
     # Your functions should return values rather than printing the result although you can use printing for testing purposes.
-    return "Your Return Value"
+    string = string.replace(",", " ") # Removes punctuation
+    x = string.split() # Splits the sentence
+    return max(x, key=len) # Returns the word with the most characters
+
 
 if __name__ == '__main__':
     # Add any code to test your solution here
